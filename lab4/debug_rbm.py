d901b4d260b6cc517903ed84e8126617e81e8f9d
chris <christopher.liu.0609@gmail.com>
2024-10-13 13:31:14 +0200

    added output folders

--- lab4/debug_rbm.py
+++ lab4/debug_rbm.py
@@ -1,17 +1,20 @@
 from util import *
 from rbm import RestrictedBoltzmannMachine 
+#from rbm_batch import RestrictedBoltzmannMachine 
 from dbn import DeepBeliefNet
 import matplotlib.pyplot as plt
 
-N_TRAIN = 5
-N_TEST = 1
-N_ITERATIONS = 5
+N_TRAIN = 60000
+N_TEST = 10000
+BATCH_SIZE = 20
+N_ITERATIONS = int(N_TRAIN/BATCH_SIZE* 1.2) 
+N_EPOCHS = 20
 
 
 
 if __name__ == "__main__":
 
-    image_size = [2,2]
+    image_size = [28,28]
     train_imgs,train_lbls,test_imgs,test_lbls = read_mnist(dim=image_size, n_train=N_TRAIN, n_test=N_TEST)
 
     ''' restricted boltzmann machine '''
@@ -25,27 +28,19 @@ if __name__ == "__main__":
                                      image_size=image_size,
                                      is_top=False,
                                      n_labels=10,
-                                     batch_size=1
+                                     batch_size=BATCH_SIZE
     )
     
     
 
-    train_imgs = np.array([
-        [1, 1, 1, 1],
-        [0.23, 0.34, 0.3, 0.2],
-        [0.8, 0.23, 0.6, 0.1],
-        [0.4, 0.2, 0.23, 0.4]
 
-    ])
 
-    print("train_imgs\n", train_imgs)
+    #print("train_imgs\n", train_imgs)
+
+    visualize_data(train_imgs)
 
     rbm.cd1(visible_trainset=train_imgs, n_iterations=N_ITERATIONS)
 
-    plt.plot(rbm.debug_weights, color="blue")
-    plt.title("Delta W for each sample")
-    plt.xlabel("Sample")
-    plt.ylabel("Delta W")
-    #plt.legend()
-    plt.savefig("out/rbm/weight_vs_it_batch_size_" + str(rbm.batch_size) + ".png")
-    #plt.show()
+
+    plot_weight_change(rbm)
+
